870b50d3430c0450d4a4b97b58376e255c905522
Kai Miyamoto <kaihogehoge@gmail.com>
2025-03-01 12:50:54 +0900

    feat: Add linting scripts and update TypeScript configuration for bid requester and builder packages

--- packages/@openrtb-bid-requester/src/index.ts
+++ packages/@openrtb-bid-requester/src/index.ts
@@ -1,5 +1,5 @@
-import { BidRequester } from "./bid-requester";
-export { BidRequester } from "./bid-requester";
-export type { IBidRequester } from "./types";
-export * from "./exception";
+import { BidRequester } from "@/bid-requester";
+export { BidRequester } from "@/bid-requester";
+export type { IBidRequester } from "@/types";
+export * from "@/exception";
 export const bidRequester = new BidRequester();
